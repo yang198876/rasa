5dc6d20053bc7d1f11b14e5123020f66cd09f64e
Tanja Bergmann <tabergma@gmail.com>
2020-04-24 14:55:27 +0200

    use transformer

--- rasa/nlu/classifiers/diet_classifier.py
+++ rasa/nlu/classifiers/diet_classifier.py
@@ -21,7 +21,6 @@ from rasa.nlu.classifiers import LABEL_RANKING_LENGTH
 from rasa.utils import train_utils
 from rasa.utils.tensorflow import layers
 from rasa.utils.tensorflow.transformer import TransformerEncoder
-from rasa.utils.tensorflow.layers import LSTMEncoder
 from rasa.utils.tensorflow.models import RasaModel
 from rasa.utils.tensorflow.model_data import RasaModelData, FeatureSignature
 from rasa.nlu.constants import (
@@ -38,9 +37,6 @@ from rasa.nlu.training_data import TrainingData
 from rasa.nlu.model import Metadata
 from rasa.nlu.training_data import Message
 from rasa.utils.tensorflow.constants import (
-    DIET_ENCODER,
-    LSTM_SIZE,
-    LSTM_LAYERS,
     LABEL,
     HIDDEN_LAYERS_SIZES,
     SHARE_HIDDEN_LAYERS,
@@ -113,10 +109,6 @@ class DIETClassifier(IntentClassifier, EntityExtractor):
 
     # please make sure to update the docs when changing a default parameter
     defaults = {
-        # FIXME: Thomas - make actual defaults from this gibberish
-        LSTM_SIZE: 256,
-        LSTM_LAYERS: 2,
-        # DIET_MODEL: "lstm",
         # ## Architecture of the used neural network
         # Hidden layer sizes for layers before the embedding layers for user message
         # and labels.
@@ -1104,42 +1096,25 @@ class DIET(RasaModel):
     def _prepare_sequence_layers(self, name: Text) -> None:
         self._prepare_input_layers(name)
 
-        # import traceback, sys
-        # traceback.print_stack(file=sys.stdout)
-        # print('=======================================================')
-
-        ## FIXME: Proof of concept to see whether it works in principle, refactor later!
-        if self.config.get(DIET_ENCODER, "transformer") == "lstm":
-            if self.config[LSTM_LAYERS] > 0:
-                self._tf_layers[f"{name}_transformer"] = LSTMEncoder(
-                    num_layers=self.config[LSTM_LAYERS],
-                    units=self.config[LSTM_SIZE],
-                    reg_lambda=self.config[REGULARIZATION_CONSTANT],
-                    dropout_rate=self.config[DROP_RATE],
-                    unidirectional=self.config[UNIDIRECTIONAL_ENCODER],
-                    name=f"{name}_encoder",
-                )
+        if self.config[NUM_TRANSFORMER_LAYERS] > 0:
+            self._tf_layers[f"{name}_transformer"] = TransformerEncoder(
+                self.config[NUM_TRANSFORMER_LAYERS],
+                self.config[TRANSFORMER_SIZE],
+                self.config[NUM_HEADS],
+                self.config[TRANSFORMER_SIZE] * 4,
+                self.config[REGULARIZATION_CONSTANT],
+                dropout_rate=self.config[DROP_RATE],
+                attention_dropout_rate=self.config[DROP_RATE_ATTENTION],
+                sparsity=self.config[WEIGHT_SPARSITY],
+                unidirectional=self.config[UNIDIRECTIONAL_ENCODER],
+                use_key_relative_position=self.config[KEY_RELATIVE_ATTENTION],
+                use_value_relative_position=self.config[VALUE_RELATIVE_ATTENTION],
+                max_relative_position=self.config[MAX_RELATIVE_POSITION],
+                name=f"{name}_encoder",
+            )
         else:
-            if self.config[NUM_TRANSFORMER_LAYERS] > 0:
-                self._tf_layers[f"{name}_transformer"] = TransformerEncoder(
-                    self.config[NUM_TRANSFORMER_LAYERS],
-                    self.config[TRANSFORMER_SIZE],
-                    self.config[NUM_HEADS],
-                    self.config[TRANSFORMER_SIZE] * 4,
-                    self.config[REGULARIZATION_CONSTANT],
-                    dropout_rate=self.config[DROP_RATE],
-                    attention_dropout_rate=self.config[DROP_RATE_ATTENTION],
-                    sparsity=self.config[WEIGHT_SPARSITY],
-                    unidirectional=self.config[UNIDIRECTIONAL_ENCODER],
-                    use_key_relative_position=self.config[KEY_RELATIVE_ATTENTION],
-                    use_value_relative_position=self.config[VALUE_RELATIVE_ATTENTION],
-                    max_relative_position=self.config[MAX_RELATIVE_POSITION],
-                    name=f"{name}_encoder",
-                )
-            else:
-                # create lambda so that it can be used later without the check
-                self._tf_layers[f"{name}_transformer"] = lambda x, mask, training: x
-        ### END OF HACK HERE
+            # create lambda so that it can be used later without the check
+            self._tf_layers[f"{name}_transformer"] = lambda x, mask, training: x
 
     def _prepare_mask_lm_layers(self, name: Text) -> None:
         self._tf_layers[f"{name}_input_mask"] = layers.InputMask()
